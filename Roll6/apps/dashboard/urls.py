e0a75fa8f687afe6538f9f3e301baf508eab89c1
cdman678 <calebthing@gmail.com>
2019-04-06 18:04:31 -0500

    added new character

--- Roll6/apps/dashboard/urls.py
+++ Roll6/apps/dashboard/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('motwfaq/', views.motw_faq, name='motwfaq'),
-    path('roll6faq/', views.roll6_faq, name='roll6faq'),
+    path('motwfaq/', views.motwfaq, name='motwfaq'),
+    path('roll6faq/', views.roll6faq, name='roll6faq'),
+    path('choosecharacter/', views.choosecharacter, name='choosecharacter'),
 ]
 
