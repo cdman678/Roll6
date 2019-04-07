786358ac20c596b31236637313271c4c3b87dbf6
cdman678 <calebthing@gmail.com>
2019-04-07 18:42:24 -0500

    dynamically load charcter fill page

--- Roll6/apps/game/urls.py
+++ Roll6/apps/game/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='game'),
     path('choosecharacter/', views.choosecharacter, name='choosecharacter'),
+    path('fill/<slug:hunter>/', views.fillsheet, name='fill-sheet'),
 ]
