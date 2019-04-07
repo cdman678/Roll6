e45769bdc86654b3d9f17d51dbbc626a0ca21d85
Michael Bishop <michael.bishop@ttu.edu>
2019-04-07 18:11:10 -0500

    Added login required to dashboard sites

--- Roll6/apps/dashboard/views.py
+++ Roll6/apps/dashboard/views.py
@@ -1,17 +1,21 @@
 from django.http import HttpResponse
 from django.template.loader import get_template
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 
+@login_required(login_url='index')
 def maindash(request):
     temp =get_template('dashboard/maindash.html')
     return HttpResponse(temp.render())
 
+@login_required(login_url='index')
 def motwfaq(request):
     temp = get_template('dashboard/motwfaq.html')
     return HttpResponse(temp.render())
 
+@login_required(login_url='index')
 def roll6faq(request):
     temp = get_template('dashboard/roll6faq.html')
     return HttpResponse(temp.render())
