9e6a095b50c7bb682150cd43919026a97fd048fb
Trenton <trentonmbailey@gmail.com>
2019-04-07 19:03:36 -0500

    Add data models

--- Roll6/apps/game/models.py
+++ Roll6/apps/game/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Game(models.Model):
-    access_key = models.CharField(max_length=4)
+    access_key = models.CharField(max_length=4,primary_key=True)
     game_name = models.CharField(max_length=30)
     keeper = models.BooleanField
     pub_date = models.DateTimeField('date published')
@@ -11,7 +11,7 @@ class Game(models.Model):
 
 
 class ActiveGames(models.Model):
-    gameID = models.CharField(max_length=4)
+    gameID = models.CharField(max_length=4,primary_key=True)
     charclass = models.CharField(max_length=20)
     charName = models.CharField(max_length= 20)
     description = models.CharField(max_length=99999)
@@ -31,28 +31,34 @@ class ActiveGames(models.Model):
 
 
 class Moves(models.Model):
-    moveid = models.IntegerField()
+    moveid = models.IntegerField(primary_key=True)
     charType= models.CharField(max_length=20)
     movename = models.CharField(max_length=20)
     description = models.CharField(max_length=1000)
     default = models.BooleanField()
     mechanics = models.CharField(max_length=100)
 
+    class Meta:
+        unique_together = (('moveid','charType'),)
+
 
 class Gear(models.Model):
-    gearID = models.IntegerField()
+    gearID = models.IntegerField(primary_key=True)
     weaponname = models.CharField(max_length=20)
     damage = models.IntegerField()
     mechanic = models.CharField(max_length=100)
 
+    class Meta:
+        unique_together = (('gearID','weaponname'),)
+
 
 class AssignedGear(models.Model):
-    charclass = models.CharField(max_length=20)
+    charclass = models.CharField(max_length=20,primary_key=True)
     gearID = models.IntegerField()
 
 
 class Ratings(models.Model):
-    ratingID = models.IntegerField()
+    ratingID = models.IntegerField(primary_key=True)
     charclass = models.CharField(max_length=20)
     charmModifier = models.IntegerField()
     coolModifier = models.IntegerField()
@@ -61,8 +67,9 @@ class Ratings(models.Model):
     weirdModifier = models.IntegerField()
 
 
+
 class Improvements(models.Model):
-    improvementID = models.IntegerField()
+    improvementID = models.IntegerField(primary_key=True)
     charclass = models.CharField(max_length=20)
     textimprovement = models.CharField(max_length=20)
     charmModifier = models.IntegerField()
@@ -72,64 +79,64 @@ class Improvements(models.Model):
     weirdModifier = models.IntegerField()
 
 
-class AdvImprovements:
-    improvementID = models.IntegerField()
+class AdvImprovements(models.Model):
+    improvementID = models.IntegerField(primary_key=True)
     charclass = models.CharField(max_length=20)
     improvement = models.CharField(max_length=100)
 
 
-class Fate:
-    fateID = models.IntegerField()
+class Fate(models.Model):
+    fateID = models.IntegerField(primary_key=True)
     heroic = models.BooleanField()
     doom = models.BooleanField()
     fatetag = models.CharField(max_length=20)
 
 
-class Heat:
-    heatID = models.IntegerField()
+class Heat(models.Model):
+    heatID = models.IntegerField(primary_key=True)
     heatDescription = models.CharField(max_length=1000)
 
 
-class Underworld:
-    underworldID = models.IntegerField()
+class Underworld(models.Model):
+    underworldID = models.IntegerField(primary_key=True)
     underworldDescription = models.CharField(max_length=1000)
 
 
-class Haven:
-    havenID = models.IntegerField()
+class Haven(models.Model):
+    havenID = models.IntegerField(primary_key=True)
     Name = models.CharField(max_length=20)
     Description = models.CharField(max_length=1000)
 
 
-class Sect:
-    sectID = models.IntegerField()
+class Sect(models.Model):
+    sectID = models.IntegerField(primary_key=True)
     good = models.BooleanField()
     bad = models.BooleanField()
     secttag = models.CharField(max_length=100)
 
 
-class Agency:
-    agencyID = models.IntegerField()
+class Agency(models.Model):
+    agencyID = models.IntegerField(primary_key=True)
     resource = models.BooleanField()
     redtape = models.BooleanField()
     option = models.CharField(max_length=20)
 
 
-class Spells:
-    spellID = models.IntegerField()
+class Spells(models.Model):
+    spellID = models.IntegerField(primary_key=True)
     base = models.BooleanField()
     effect = models.BooleanField()
     spellname = models.CharField(max_length=20)
     spelldescription = models.CharField(max_length=1000)
 
 
-class DarkSide:
-    tagID = models.IntegerField()
+class DarkSide(models.Model):
+    tagID = models.IntegerField(primary_key=True)
     tag = models.CharField(max_length=20)
 
 
-class Lost:
-    id = models.IntegerField()
+class Lost(models.Model):
+    id = models.IntegerField(primary_key=True)
     who = models.BooleanField()
     what = models.BooleanField()
     description = models.CharField(max_length=1000)
