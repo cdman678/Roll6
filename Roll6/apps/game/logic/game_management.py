85bb923f5ab95431161bb3e7f74870a73fca49c7
The_Fireplace <the.f1repl4ce@gmail.com>
2019-04-07 21:45:48 -0500

    Handle empty string as a way to get all moves, add getters for more things from the database

--- Roll6/apps/game/logic/game_management.py
+++ Roll6/apps/game/logic/game_management.py
@@ -3,8 +3,20 @@ from django.db.models import Q
 from Roll6.apps.game.models import *
 
 
-def get_moves(character_type):
-    return Moves.objects.filter(Q(char_class__char_class=character_type))
+def get_moves(character_type=""):
+    return Moves.objects.filter(Q(char_class__char_class=character_type)) if character_type else Moves.objects.get()
+
+
+def get_gear(character_type=""):
+    return AssignedGear.objects.filter(Q(char_class__char_class=character_type)) if character_type else AssignedGear.objects.get()
+
+
+def get_improvements(character_type=""):
+    return Improvements.objects.filter(Q(char_class__char_class=character_type)) if character_type else Improvements.objects.get()
+
+
+def get_adv_improvements(character_type=""):
+    return AdvImprovements.objects.filter(Q(char_class__char_class=character_type)) if character_type else AdvImprovements.objects.get()
 
 
 def create_new_game(game_name):
