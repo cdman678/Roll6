c5cfd4ddb3b037fcd5c17a311e62ea3e5f43a50a
The_Fireplace <the.f1repl4ce@gmail.com>
2019-04-07 20:05:10 -0500

    Document string to list function

--- Roll6/apps/game/logic/parsing.py
+++ Roll6/apps/game/logic/parsing.py
@@ -1,4 +1,8 @@
-def stringToList(string):
+def string_to_list(string):
+    """
+    :param string: The string to split, should be a comma separated list
+    :return: the list of strings
+    """
     out = []
     if isinstance(string, str):
         out = string.split(',')
