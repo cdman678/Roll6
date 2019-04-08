825be2dd1bcb270b3fa6d55fc794c41449702531
The_Fireplace <the.f1repl4ce@gmail.com>
2019-04-07 21:09:43 -0500

    Fix get moves

--- Roll6/apps/game/logic/game_management.py
+++ Roll6/apps/game/logic/game_management.py
@@ -3,7 +3,7 @@ from django.db.models import Q
 
 
 def get_moves(character_type):
-    return Moves.objects.filter(Q(char_class=character_type))
+    return Moves.objects.filter(Q(char_class__char_class=character_type))
 
 
 def create_new_game(game_name):
