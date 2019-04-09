3ff2f691d4fa4838525d33994220c0ea26e7ae03
cdman678 <calebthing@gmail.com>
2019-04-08 19:26:45 -0500

    displaying weapon options

--- Roll6/apps/game/logic/game_management.py
+++ Roll6/apps/game/logic/game_management.py
@@ -1,5 +1,7 @@
-from django.db.models import Q
 import random
+import re
+
+from django.db.models import Q
 
 from Roll6.apps.game.models import *
 
@@ -11,6 +13,13 @@ def get_moves(character_type=""):
 def get_gear(character_type=""):
     return AssignedGear.objects.filter(Q(char_class__char_class=character_type)) if character_type else AssignedGear.objects.get()
 
+def get_gear_info(gear_ID):
+    return Gear.objects.filter(Q(gear_ID=gear_ID)if gear_ID else Gear.objects.get())
+
+
+def get_ratings(character_type=""):
+    return Ratings.objects.filter(Q(char_class__char_class=character_type)) if character_type else Ratings.objects.get()
+
 
 def get_improvements(character_type=""):
     return Improvements.objects.filter(Q(char_class__char_class=character_type)) if character_type else Improvements.objects.get()
@@ -18,6 +27,12 @@ def get_improvements(character_type=""):
 
 def get_adv_improvements(character_type=""):
     return AdvImprovements.objects.filter(Q(char_class__char_class=character_type)) if character_type else AdvImprovements.objects.get()
+
+def fix_id(old_gear_ID ):
+    return_temp = re.findall("\d[0-9]*", old_gear_ID)
+    if len(return_temp) == 1:
+        return return_temp[0]
+
 
 
 def generate_game_id():
--- Roll6/apps/game/views.py
+++ Roll6/apps/game/views.py
@@ -6,7 +6,6 @@ from Roll6.apps.game.logic.game_management import *
 
 # Create your views here.
 
-
 def index(request):
     t = get_template('game/game.html')
     return HttpResponse(t.render())
@@ -19,7 +18,22 @@ def fillsheet(request, hunter):
     if request.method == 'POST':
         print (request.POST)
         return HttpResponse("Hello")
-    #returns lists of rows
     else:
         move_list = get_moves(hunter)
-        return render(request, 'game/fillsheet.html', {'type': hunter,'move_list': move_list})
+        avaliable_gear_list = get_gear(hunter)
+        gear_temp = []
+        for gear in avaliable_gear_list:
+            temp = fix_id(str(gear.gear_ID))
+            gear_temp.append(get_gear_info(int(temp)))
+        rating_list = get_ratings(hunter)
+
+        gear_info = []
+        for queary in gear_temp:
+            for gear in queary:
+                gear_info.append(gear)
+
+
+        return render(request, 'game/fillsheet.html', {'type': hunter,
+                                                       'move_list': move_list,
+                                                       'gear_info': gear_info,
+                                                       'rating_list': rating_list})
