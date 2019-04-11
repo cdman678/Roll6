6996ac96e2600e0a9305d30781c39b0d30abcaed
Trenton <trentonmbailey@gmail.com>
2019-04-11 01:53:02 -0500

    fix link

--- Roll6/apps/game/urls.py
+++ Roll6/apps/game/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='create-game'),
     path('create/', views.create_game, name='create-game'),
     path('join/', views.join_game, name='join-game'),
     path(r'<slug:gameid>/choosecharacter/', views.choosecharacter, name='choosecharacter'),
